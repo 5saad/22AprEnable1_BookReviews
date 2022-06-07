3f7d0b479dc5a8980d3e2dbf5199e0b032b0d7ca
Arcnisity <66567037+Arcnisity@users.noreply.github.com>
2022-06-07 09:36:59 +0100

    many to one/ one to many realationship added

--- src/main/java/com/qa/starterproject/domain/Review.java
+++ src/main/java/com/qa/starterproject/domain/Review.java
@@ -4,6 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +27,10 @@ public class Review {
 	private String surname;
 	private int rating;
 	private String review;
+	
+	@ManyToOne
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonBackReference
+	@JoinColumn(name="book_id")
+    private Book book; 
 }

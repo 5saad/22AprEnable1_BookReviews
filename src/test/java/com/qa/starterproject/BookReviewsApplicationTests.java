9e1e91df41537427f44e4aed13274f92187a8a12
Arcnisity <66567037+Arcnisity@users.noreply.github.com>
2022-06-10 11:41:11 +0100

    smells reduced

--- src/test/java/com/qa/starterproject/BookReviewsApplicationTests.java
+++ src/test/java/com/qa/starterproject/BookReviewsApplicationTests.java
@@ -1,13 +1,15 @@
 package com.qa.starterproject;
 
-import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookReviewsApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewsApplicationTests.class, args);
 	}
+		
 
 }
